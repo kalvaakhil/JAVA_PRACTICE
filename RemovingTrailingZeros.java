6387efcbb924b41b8dd86cb045766be64fe3c585
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2024-11-25 15:33:30 +0530

    Add files via upload

--- RemovingTrailingZeros.java
+++ RemovingTrailingZeros.java
@@ -1,9 +1,26 @@
 package basics;
 
 public class RemovingTrailingZeros {
+	
+	public String getString(String str)
+	{
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			if(str.charAt(i)!='0')
+			{
+				return str.substring(0,i+1);
+			}
+		}
+		return "";
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String str="Akhil000";
+		RemovingTrailingZeros removingTrailingZeros=new RemovingTrailingZeros();
+		String ans=removingTrailingZeros.getString(str);
+		System.out.println(ans);
+		
 
 	}
 
